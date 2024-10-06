7c985ab8534345d79b85d40bd6e60a6e16529208
Dmitrii <dmitrii_zadorozhnyi@epam.com>
2024-10-06 20:32:22 +0500

    add flash message when creating new blog post

--- src/Controller/Admin/BlogController.php
+++ src/Controller/Admin/BlogController.php
@@ -10,6 +10,7 @@ use App\Message\CheckUniqueTextJob;
 use App\MessageHandler\CheckUniqueTextJobHandler;
 use App\Repository\BlogRepository;
 use App\Service\CheckUniqueText;
+use App\Service\MessageGenerator;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,105 +24,111 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/admin/blog')]
 final class BlogController extends AbstractController
 {
-  #[Route(name: 'app_blog_index', methods: ['GET'])]
-  public function index(Request            $request,
-                        BlogRepository     $blogRepository,
-                        PaginatorInterface $paginator,
-  ): Response
-  {
+    #[Route(name: 'app_blog_index', methods: ['GET'])]
+    public function index(Request            $request,
+                          BlogRepository     $blogRepository,
+                          PaginatorInterface $paginator,
+    ): Response
+    {
 
-    //dd($blogRepository->findAll());
-    $blogFilter = new BlogFilter();
-    $form = $this->createForm(BlogFilterType::class, $blogFilter);
-    $form->handleRequest($request);
-    //dd($blogFilter);
+        //dd($blogRepository->findAll());
+        $blogFilter = new BlogFilter();
+        $form = $this->createForm(BlogFilterType::class, $blogFilter);
+        $form->handleRequest($request);
+        //dd($blogFilter);
 
-    $pagination = $paginator->paginate(
-    //$query, /* query NOT result */
-      $blogRepository->findByBlogFilter($blogFilter),
-      $request->query->getInt('page', 1), /*page number*/
-      5 /*limit per page*/
-    );
+        $pagination = $paginator->paginate(
+        //$query, /* query NOT result */
+            $blogRepository->findByBlogFilter($blogFilter),
+            $request->query->getInt('page', 1), /*page number*/
+            5 /*limit per page*/
+        );
 
-    return $this->render('blog/index.html.twig', [
-      //'blogs' => $blogRepository->findByBlogFilter($blogFilter),
-      'pagination' => $pagination,
-      'formSearch' => $form->createView(),
-    ]);
-  }
+        return $this->render('blog/index.html.twig', [
+            //'blogs' => $blogRepository->findByBlogFilter($blogFilter),
+            'pagination' => $pagination,
+            'formSearch' => $form->createView(),
+        ]);
+    }
 
-  #[Route('/new', name: 'app_blog_new', methods: ['GET', 'POST'])]
-  public function new(Request $request,
-                      EntityManagerInterface $entityManager,
-                      /*CheckUniqueText $checkUniqueText*/
-                      MessageBusInterface $bus): Response
-  {
-    $blog = new Blog($this->getUser());
-    $form = $this->createForm(BlogType::class, $blog);
-    $form->handleRequest($request);
+    #[Route('/new', name: 'app_blog_new', methods: ['GET', 'POST'])]
+    public function new(Request                $request,
+                        EntityManagerInterface $entityManager,
+        /*CheckUniqueText $checkUniqueText*/
+                        MessageBusInterface    $bus,
+                        MessageGenerator       $messageGenerator): Response
+    {
+        $blog = new Blog($this->getUser());
+        $form = $this->createForm(BlogType::class, $blog);
+        $form->handleRequest($request);
 
-    if ($form->isSubmitted() && $form->isValid()) {
-      $entityManager->persist($blog);
-      $entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($blog);
+            $entityManager->flush();
 
-      // TODO проверка уникальности пока здесь далее будет перенесена в очередь,
-      // поскольку метод checkUniqueText использует curl и может быть причиной замедления
+            // TODO проверка уникальности пока здесь далее будет перенесена в очередь,
+            // поскольку метод checkUniqueText использует curl и может быть причиной замедления
 //      $unique_percent = $checkUniqueText->checkUniqueText($blog->getDescription());
 //      $blog->setPercent($unique_percent);
 
-      // ВРЕМЕННО закомментить
-      $bus->dispatch(new CheckUniqueTextJob($blog->getId()));
+            // ВРЕМЕННО закомментить
+            $bus->dispatch(new CheckUniqueTextJob($blog->getId()));
 
-      $entityManager->flush();
-      return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+            $entityManager->flush();
+
+            $message = $messageGenerator->getHappyMessage();
+            $message .= " Blog post with ID= {$blog->getId()} is created";
+            $this->addFlash('success', $message);
+
+            return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('blog/new.html.twig', [
+            'blog' => $blog,
+            'form' => $form,
+        ]);
     }
 
-    return $this->render('blog/new.html.twig', [
-      'blog' => $blog,
-      'form' => $form,
-    ]);
-  }
-
-  #[Route('/{id}', name: 'app_blog_show', methods: ['GET'])]
-  public function show(Blog $blog): Response
-  {
-    $blog_tags = [];
-    foreach ($blog->getTags() as $tag) {
-      $blog_tags[] = $tag->getName();
-    }
-    return $this->render('blog/show.html.twig', [
-      'blog' => $blog,
-      'blog_tags' => $blog_tags,
-    ]);
-  }
-
-  #[IsGranted('edit', 'blog')]
-  #[Route('/{id}/edit', name: 'app_blog_edit', methods: ['GET', 'POST'])]
-  public function edit(Request $request, Blog $blog, EntityManagerInterface $entityManager): Response
-  {
-    $form = $this->createForm(BlogType::class, $blog);
-    $form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-      $entityManager->flush();
-
-      return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+    #[Route('/{id}', name: 'app_blog_show', methods: ['GET'])]
+    public function show(Blog $blog): Response
+    {
+        $blog_tags = [];
+        foreach ($blog->getTags() as $tag) {
+            $blog_tags[] = $tag->getName();
+        }
+        return $this->render('blog/show.html.twig', [
+            'blog' => $blog,
+            'blog_tags' => $blog_tags,
+        ]);
     }
 
-    return $this->render('blog/edit.html.twig', [
-      'blog' => $blog,
-      'form' => $form,
-    ]);
-  }
+    #[IsGranted('edit', 'blog')]
+    #[Route('/{id}/edit', name: 'app_blog_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Blog $blog, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(BlogType::class, $blog);
+        $form->handleRequest($request);
 
-  #[Route('/{id}', name: 'app_blog_delete', methods: ['POST'])]
-  public function delete(Request $request, Blog $blog, EntityManagerInterface $entityManager): Response
-  {
-    if ($this->isCsrfTokenValid('delete' . $blog->getId(), $request->getPayload()->getString('_token'))) {
-      $entityManager->remove($blog);
-      $entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('blog/edit.html.twig', [
+            'blog' => $blog,
+            'form' => $form,
+        ]);
     }
 
-    return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
-  }
+    #[Route('/{id}', name: 'app_blog_delete', methods: ['POST'])]
+    public function delete(Request $request, Blog $blog, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $blog->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($blog);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_blog_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
