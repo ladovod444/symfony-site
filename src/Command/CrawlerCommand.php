ccbe38afc65ca7c0401504dacbf8b7bf417d2be2
Dmitrii <ladovod@gmail.com>
2024-10-07 15:56:41 +0500

    add fixes to Blog entity class, NewsGrabber service and CrawlerCommand

--- src/Command/CrawlerCommand.php
+++ src/Command/CrawlerCommand.php
@@ -5,6 +5,7 @@ namespace App\Command;
 use App\Service\NewsGrabber;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -13,40 +14,55 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
-    name: 'blog:news:import',
-    description: 'Import news from internet',
+  name: 'blog:news:import',
+  description: 'Import news from internet',
 )]
 class CrawlerCommand extends Command
 {
-    public function __construct(private readonly NewsGrabber $newsGrabber)
-    {
-        parent::__construct();
-    }
+  use LockableTrait;
 
-    protected function configure(): void
-    {
-        $this->addArgument('count', InputArgument::OPTIONAL, 'Number of news')
-            ->addOption('dryRun', null, InputOption::VALUE_OPTIONAL);
+  public function __construct(private readonly NewsGrabber $newsGrabber)
+  {
+    parent::__construct();
+  }
+
+  protected function configure(): void
+  {
+    $this->addArgument('count', InputArgument::OPTIONAL, 'Number of news')
+      ->addOption('dryRun', null, InputOption::VALUE_OPTIONAL);
 //        $this
 //            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
 //            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
 //        ;
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output): int
+  {
+
+    if (!$this->lock()) {
+      $output->writeln('The command is already running in another process.');
+      return Command::SUCCESS;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        //$io->success('You have a new command! Now make it your own! Pass --help to see your options.');
-        // Получение аргументов
-        $count = $input->getArgument('count');
-        //$dryRun =(bool)$input->getOption('dryRun');
-        $dryRun =(bool)$input->getOption('dryRun');
+    //sleep(10000);
+
+    //$io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+    // Получение аргументов
+    $count = $input->getArgument('count');
+    //$dryRun =(bool)$input->getOption('dryRun');
+    $dryRun = (bool)$input->getOption('dryRun');
 //        echo $dryRun;
 //        echo $count;
 //        return Command::SUCCESS;
 
-        $logger = new ConsoleLogger($output);
-        $this->newsGrabber->setLogger($logger)->importNews($count, $dryRun);
-        //echo "test10000000\n";
-        return Command::SUCCESS;
-    }
+    $logger = new ConsoleLogger($output);
+    $output->writeln('<info>Importing news ...</info>');
+    $output->writeln('<error>Some error while importing news ...</error>');
+    $output->writeln('<comment>Comment ...</comment>');
+    $this->newsGrabber->setLogger($logger)->importNews($count, $dryRun);
+    //echo "test10000000\n";
+
+    $this->release();
+    return Command::SUCCESS;
+  }
 }
--- src/Entity/Blog.php
+++ src/Entity/Blog.php
@@ -57,7 +57,7 @@ class Blog
   #[ORM\JoinColumn(name: 'category_id', referencedColumnName: 'id')]
   private Category|null $category = null;
 
-  #[ORM\ManyToOne(targetEntity: User::class)]
+  #[ORM\ManyToOne(targetEntity: User::class, cascade: ['persist'])]
   #[ORM\JoinColumn(name: 'user_id', referencedColumnName: 'id')]
   #[Groups(['blog:list', 'blog:item'])]
   private User|null $user = null;
