294aa524c7fd8354d399510aaa3053ad67c73a42
Dmitrii <ladovod@gmail.com>
2024-10-07 15:53:11 +0500

    add author_blog parameter to service, added api package

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
