3861203c13a712d893168029acc5a1afee56e40f
Dmitrii <ladovod@gmail.com>
2024-10-09 22:51:42 +0500

    disable crsf_protection for BlogType form

--- src/Form/BlogType.php
+++ src/Form/BlogType.php
@@ -108,7 +108,7 @@ class BlogType extends AbstractType
   {
     $resolver->setDefaults([
       'data_class' => Blog::class,
-      //'csrf_protection' => false,
+      'csrf_protection' => false,
     ]);
   }
 }
