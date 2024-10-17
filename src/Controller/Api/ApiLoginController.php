515bf62f4e88c5cb72c051724b4ec0af071a02cd
Dmitrii <ladovod@gmail.com>
2024-10-17 16:21:47 +0500

    Add custom JWT to json_login

--- src/Controller/Api/ApiLoginController.php
+++ src/Controller/Api/ApiLoginController.php
@@ -3,21 +3,32 @@
 namespace App\Controller\Api;
 
 use App\Entity\User;
+use App\Service\CustomJwtJsonCheck;
+use JetBrains\PhpStorm\NoReturn;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class ApiLoginController extends AbstractController
 {
-  #[Route('/api/login', 'api_login', methods: ['POST'])]
-  public function index(#[CurrentUser] ?User $user): Response {
-
-    //dd($user);
+  #[NoReturn]
+  #[Route('/api/login', 'api_login_json', methods: ['POST'])]
+  public function index(#[CurrentUser] ?User $user, CustomJwtJsonCheck $customJwtJsonCheck): Response {
+    if (null === $user) {
+      return $this->json([
+        'message' => 'missing creds',
+        'file' => __FILE__
+      ]/*, Response::HTTP_UNAUTHORIZED*/);
+    }
+    
     return $this->json([
-      'message' => 'Logged in successfully.',
-      'file' => __FILE__
+      'message' => 'Result: Success',
+      'jwt' => $customJwtJsonCheck->getJwt($user)
     ]);
   }
 }
